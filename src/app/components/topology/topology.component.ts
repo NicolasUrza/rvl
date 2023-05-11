2565d00577d398a2a153842aa817b918a7e98d6e
NicolasUrza <88108043+NicolasUrza@users.noreply.github.com>
2023-05-10 22:58:49 -0300

    added movement and limits

--- src/app/components/topology/topology.component.ts
+++ src/app/components/topology/topology.component.ts
@@ -21,36 +21,37 @@ export class TopologyComponent {
   FiguresClick() {
     // se muestra o se oculta la descripcion
     this.FiguresActive = !this.FiguresActive;
-    if (!this.FiguresActive) {
-      //si se oculta la descripcion se espera 300ms y se pone la altura a 0
-      setTimeout(() => {
-        let element = document.getElementById("figures_box");
-        element.style.height = "0";
-      }, 300);
-    }
-    else {
-      let element = document.getElementById("figures_box");
-      element.style.height = "auto";
-    }
+    this.DevicesActive = false;
+    this.ConnectionsActive = false;
+    this.CloseInactiveMenus();
   }
   DevicesClick() {
     // se muestra o se oculta la descripcion
     this.DevicesActive = !this.DevicesActive;
-    if (!this.DevicesActive) {
-      //si se oculta la descripcion se espera 300ms y se pone la altura a 0
-      setTimeout(() => {
-        let element = document.getElementById("devices_box");
-        element.style.height = "0";
-      }, 300);
-    }
-    else {
-      let element = document.getElementById("devices_box");
-      element.style.height = "auto";
-    }
+    this.FiguresActive = false;
+    this.ConnectionsActive = false;
+    this.CloseInactiveMenus();
+    
   }
   ConnectionsClick() {
     // se muestra o se oculta la descripcion
     this.ConnectionsActive = !this.ConnectionsActive;
+    this.DevicesActive = false;
+    this.FiguresActive = false;
+    this.CloseInactiveMenus();
+  }
+  @ViewChild('componentContainer', { read: ViewContainerRef }) componentContainer: ViewContainerRef;
+
+  CreateItem(imgSource:string, name:string, event:MouseEvent){
+    this.numberOfCreations.Count(name);
+    const factory = this.componentFactoryResolver.resolveComponentFactory(ItemComponent);
+    const componentRef = this.componentContainer.createComponent(factory);
+    componentRef.instance.topology = new TopologyObjectBuilder().build(name + this.numberOfCreations.GetCount(name).toString(), imgSource);
+    this.componentContainer.insert(componentRef.hostView);
+  
+  }
+
+  CloseInactiveMenus(){
     if (!this.ConnectionsActive) {
       //si se oculta la descripcion se espera 300ms y se pone la altura a 0
       setTimeout(() => {
@@ -62,16 +63,29 @@ export class TopologyComponent {
       let element = document.getElementById("connections_box");
       element.style.height = "auto";
     }
-  }
-  @ViewChild('componentContainer', { read: ViewContainerRef }) componentContainer: ViewContainerRef;
 
-  CreateItem(imgSource:string, name:string, event:MouseEvent){
-    this.numberOfCreations.Count(name);
-    const factory = this.componentFactoryResolver.resolveComponentFactory(ItemComponent);
-    const componentRef = this.componentContainer.createComponent(factory);
-    componentRef.instance.topology = new TopologyObjectBuilder().build(name + this.numberOfCreations.GetCount(name).toString(), imgSource);
-    this.componentContainer.insert(componentRef.hostView);
-  
+    if (!this.DevicesActive) {
+      //si se oculta la descripcion se espera 300ms y se pone la altura a 0
+      setTimeout(() => {
+        let element = document.getElementById("devices_box");
+        element.style.height = "0";
+      }, 300);
+    }
+    else {
+      let element = document.getElementById("devices_box");
+      element.style.height = "auto";
+    }
+    if (!this.FiguresActive) {
+      //si se oculta la descripcion se espera 300ms y se pone la altura a 0
+      setTimeout(() => {
+        let element = document.getElementById("figures_box");
+        element.style.height = "0";
+      }, 300);
+    }
+    else {
+      let element = document.getElementById("figures_box");
+      element.style.height = "auto";
+    }
   }
 
 
